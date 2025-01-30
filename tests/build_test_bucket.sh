4c5b795bb511d77f19030ee98af29e042420768e
Ignacio Cuevas <ignacio.cuevas@mpi-sp.org>
2025-01-30 13:25:55 +0100

    test(script): automatically adjust params

--- tests/build_test_bucket.sh
+++ tests/build_test_bucket.sh
@@ -1,5 +1,6 @@
 rm build/*; \
+sed -i 's/^param int PATH_LENGTH = [0-9]\+;/param int PATH_LENGTH = 11;/' jasmin/params.jinc && \
 jasminc -nowarning -o build/jtree_path.s jasmin/jtree_path.jazz && \
 jasminc -nowarning -o build/jbucket.s jasmin/jbucket.jazz && \
-gcc -Wall -Wextra -g -O3 -fomit-frame-pointer -o build/test_bucket src/bucket.c src/tree_path.c build/jbucket.s build/jtree_path.s tests/test_bucket.c -DIS_TEST && \
+gcc -DIS_TEST -Wall -Wextra -g -O3 -fomit-frame-pointer -o build/test_bucket src/bucket.c src/tree_path.c build/jbucket.s build/jtree_path.s tests/test_bucket.c && \
 ./build/test_bucket
--- tests/build_test_stash.sh
+++ tests/build_test_stash.sh
@@ -1,6 +1,7 @@
 rm build/*; \
-/home/mpi-sp-it/jasmin/compiler/jasminc -nowarning -o build/jtree_path.s jasmin/jtree_path.jazz && \
-/home/mpi-sp-it/jasmin/compiler/jasminc -nowarning -o build/jbucket.s jasmin/jbucket.jazz && \
-/home/mpi-sp-it/jasmin/compiler/jasminc -nowarning -o build/jstash.s jasmin/jstash.jazz && \
-gcc -Wall -Wextra -g -O3 -fomit-frame-pointer -o build/test_stash src/bucket.c src/tree_path.c src/stash.c build/jbucket.s build/jtree_path.s build/jstash.s tests/test_stash.c && \
+sed -i 's/^param int PATH_LENGTH = [0-9]\+;/param int PATH_LENGTH = 18;/' jasmin/params.jinc && \
+jasminc -nowarning -o build/jtree_path.s jasmin/jtree_path.jazz && \
+jasminc -nowarning -o build/jbucket.s jasmin/jbucket.jazz && \
+jasminc -nowarning -o build/jstash.s jasmin/jstash.jazz && \
+gcc -DIS_TEST -Wall -Wextra -g -O3 -fomit-frame-pointer -o build/test_stash src/bucket.c src/tree_path.c src/stash.c build/jbucket.s build/jtree_path.s build/jstash.s tests/test_stash.c && \
 ./build/test_stash
