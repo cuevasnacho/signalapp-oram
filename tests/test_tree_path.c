4d5bcb6e1507bbd6be5cfc65e028c85142320efa
Ignacio Cuevas <ignacio.cuevas@mpi-sp.org>
2025-02-18 10:29:19 +0100

    fix: remove tree length parameter to make more flexible

--- tests/test_tree_path.c
+++ tests/test_tree_path.c
@@ -27,6 +27,9 @@ int test_path(u64 leaf, u64 root, size_t expected_len, const u64 expected_path[e
 
 int test_paths()
 {
+    u64 e0[] = {10, 9, 11, 7};
+    test_path(10, 7, 4, e0);
+
     u64 e1[] = {2046, 2045, 2043, 2039, 2031, 2015, 1983, 1919, 1791, 1535, 1023};
     test_path(2046, 1023, 11, e1);
 
