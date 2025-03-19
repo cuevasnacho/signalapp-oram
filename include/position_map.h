6550a5edb0c470f5095eb3f80bbec634aefe891d
Ignacio Cuevas <ignacio.cuevas@mpi-sp.org>
2025-03-19 10:03:53 +0100

    fix: add ct mod and division for block_id

--- include/position_map.h
+++ include/position_map.h
@@ -9,7 +9,7 @@
 #define POSITION_MAP_NOT_PRESENT UINT64_MAX
 #define SCAN_THRESHOLD (1 << 14)
 
-typedef u64 position_map[6];
+typedef u64 position_map[10];
 
 /**
  * @brief The `position_map` is used internally by an ORAM to keep track of the current physical
